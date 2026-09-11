67937e9cb78d6e414c5b34bff6c98f55c5ececed
Bogdan2112 <cozmolicibogdan@gmail.com>
2026-09-11 17:08:14 +0300

    Week model, contrtoller and route api

---
+++ app/Http/Controllers/WeekController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Week;
+
+class WeekController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $weeks = Week::all();
+
+        return response()->json($weeks);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+         $request->validate([
+            'name' => 'required|string|max:255'
+        ]);
+
+        $week = Week::create([
+            'name' => $request->name,
+            'user_id' => 1
+        ]);
+
+        return response()->json($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $week = Week::find($id);
+
+        return response()->json($week);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $week = Week::find($id);
+
+        $request->validate([
+            'name' => 'required|string|max:255'
+        ]);
+
+        $week->name = $request->name;
+        $week->save();
+
+        return response()->json($week);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WeekController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/weeks', [WeekController::class, 'index']);
+Route::post('/weeks', [WeekController::class, 'store']);
+Route::get('/weeks/{id}',[WeekController::class, 'show']);  
+Route::put('/weeks/{id}',[WeekController::class, 'update']);
